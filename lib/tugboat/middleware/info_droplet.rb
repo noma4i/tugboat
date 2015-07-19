9fe194c21c441d565904f2b3d1b3bb8cdb41cbd1
Peter Souter <peter.souter@puppetlabs.com>
2015-07-19 20:43:17 +0100

    Moving error messages to new API

--- lib/tugboat/middleware/info_droplet.rb
+++ lib/tugboat/middleware/info_droplet.rb
@@ -4,20 +4,22 @@ module Tugboat
       def call(env)
         ocean = env['barge']
 
-        req = ocean.droplet.show env["droplet_id"]
+        response = ocean.droplet.show env["droplet_id"]
 
-        if req.status == "ERROR"
-          say "#{req.status}: #{req.error_message}", :red
+        if response.success?
+          say "done", :green
+        else
+          say "Failed to get infor for Droplet: #{response.message}", :red
           exit 1
         end
 
-        droplet = req.droplet
+        droplet = response.droplet
 
-          if droplet.status == "active"
-            status_color = GREEN
-          else
-            status_color = RED
-          end
+        if droplet.status == "active"
+          status_color = GREEN
+        else
+          status_color = RED
+        end
 
         say
         say "Name:             #{droplet.name}"
@@ -32,7 +34,7 @@ module Tugboat
 
         say "Region:           #{droplet.region.name} - #{droplet.region.slug}"
         say "Image:            #{droplet.image.id} - #{droplet.image.name}"
-        say "Size:             #{droplet.size_slug.upcase}"
+        say "Size:             #{droplet.size_slug}"
         say "Backups Active:   #{!droplet.backup_ids.empty?}"
 
         @app.call(env)
